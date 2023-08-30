4cd483524f126f2623302e5e84f243fc60c66936
Klaulara <claudia_lara_zamora@hotmail.com>
2023-08-29 20:23:20 -0400

    vista calculo de cuota

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>PROYECTO DIECIOCHERO</h1>
+    <H2>Calcula la cuota para tu asado</H2>
+
+    <form class="container">
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label"
+          >Cantidad de personas</label
+        >
+        <input
+          type="number"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="10"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label"
+          >Cantidad de carne a consumir</label
+        >
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Elije cantidad</option>
+          <option value="1">200 gramos</option>
+          <option value="2">300 gramos</option>
+          <option value="3">500 gramos</option>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label"
+          >Cantidad de pollo a consumir</label
+        >
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Elije cantidad</option>
+          <option value="1">200 gramos</option>
+          <option value="2">300 gramos</option>
+          <option value="3">500 gramos</option>
+        </select>
+      </div>
+      <input type="submit" value="Calcular" />
+    </form>
+
+    <div class="mt-5">
+      <h3>Resultado</h3>
+      <p>El valor de la cuota es de $ 10.000</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
